aa5209e0cbfecd57854e040af0d7a8c8f0cc1233
I-M-Marinov <lcfrrr@gmail.com>
2023-09-12 13:44:36 -0500

    Add project files.

---
+++ P2.StackSum/Program.cs
@@ -0,0 +1,37 @@
+﻿
+List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+Stack<int> stack = new Stack<int>(input);
+
+string command = Console.ReadLine().ToLower();
+
+while (command != "end")
+{
+    string[] split = command .Split(' ');
+    if (command.Contains("add"))
+    {
+        int first = int.Parse(split[1]);
+        int second = int.Parse(split[2]);
+        stack.Push(first);
+        stack.Push(second);
+    }
+    else if (command.Contains("remove"))
+    {
+        int n = int.Parse(split[1]);
+        if (n <= stack.Count)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                stack.Pop();
+            }
+        }
+    }
+    command = Console.ReadLine().ToLower();
+}
+int sum = 0;
+
+while (stack.Count > 0)
+{
+    sum += stack.Pop();
+}
+Console.WriteLine($"Sum: {sum}");
---
+++ P7.HotPotato/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection.Metadata;
+
+Queue<string> children = new Queue<string>(Console.ReadLine().Split());
+
+int tossCount = int.Parse(Console.ReadLine());
+int tosses = 0;
+while (children.Count > 1)
+{
+    string childWithPotato = children.Dequeue();
+    tosses++;
+
+    if (tosses == tossCount)
+    {
+        tosses = 0;
+        Console.WriteLine($"Removed {childWithPotato}");
+    }
+    else
+    {
+        children.Enqueue(childWithPotato);
+    }
+}
+
+Console.WriteLine($"Last is {children.Dequeue()}");
+
+
+
