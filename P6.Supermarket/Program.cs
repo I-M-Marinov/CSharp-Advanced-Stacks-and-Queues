2e2efd6b74dee798194b4df7364972cc1f23dba5
I-M-Marinov <lcfrrr@gmail.com>
2023-09-14 09:42:03 -0500

    P6. Supermarket
    Added solution to P6. Supermarket

---
+++ P6.Supermarket/Program.cs
@@ -0,0 +1,26 @@
+﻿string input = Console.ReadLine();
+
+Queue<string> customerQueue = new Queue<string>();
+
+while (input != "End")
+{
+    if (input != "Paid" && input != "End")
+    {
+        customerQueue.Enqueue(input);
+    }
+    else if (input == "Paid")
+    {
+        while (customerQueue.Count > 0)
+        {
+            Console.WriteLine($"{customerQueue.Dequeue()}");
+        }
+    }
+
+    input = Console.ReadLine();
+
+    if (input == "End")
+    {
+        Console.WriteLine($"{customerQueue.Count} people remaining.");
+    }
+}
+
